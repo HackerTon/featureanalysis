8a9f964209732e7c521330b10d37b5ba94c23ef5
hackerton <9619080+HackerTon@users.noreply.github.com>
2023-03-11 17:17:21 +0800

    Bugfix. dice_loss does not return [B, C]

--- metrics.py
+++ metrics.py
@@ -1,18 +1,26 @@
 import tensorflow as tf
 
 
-# Mode must OUTPUT in logits [-inf, inf]
-# Make sure input dimension is [B, H, W, C]
 def jindex_class(target, pred):
+    '''
+    Mode must OUTPUT in logits [-inf, inf]
+    Make sure input dimension is [B, H, W, C]
+    Should return [B, C]
+    '''
     intersection = tf.reduce_sum(target * pred, axis=[1, 2])
     union = tf.reduce_sum(target + pred, axis=[1, 2]) - intersection
-    return tf.reduce_mean((intersection + 1e-9) / (union + 1e-9), axis=0)
+    return (intersection + 1e-9) / (union + 1e-9)
 
 
 def dice_coef(target, pred):
+    '''
+    Mode must OUTPUT in logits [-inf, inf]
+    Make sure input dimension is [B, H, W, C]
+    Should return [B, C]
+    '''
     nominator = 2 * tf.reduce_sum(target * pred, axis=[1, 2])
     denominator = tf.reduce_sum(target + pred, axis=[1, 2])
-    return tf.reduce_mean((nominator + 1e-9) / (denominator + 1e-9), axis=0)
+    return (nominator + 1e-9) / (denominator + 1e-9)
 
 
 def dice_loss(target, pred):
@@ -20,4 +28,8 @@ def dice_loss(target, pred):
     This loss does not reduce batch dimension
     Therefore, the output os [B, 1]
     """
-    return 1 - dice_coef(target, pred)
+    tf.print()
+    return 1 - tf.reduce_mean(dice_coef(target, pred), axis=-1)
+
+
+

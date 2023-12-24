1e890ac04b19e3f36d607e6814cff713c1fb8476
hackerton <9619080+HackerTon@users.noreply.github.com>
2023-12-24 14:13:06 +0800

    Implement adjustable epoch

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from service.hyperparamater import Hyperparameter
 from service.trainer import Trainer
 
 
-def run(device: str, batch_size: int, path: str):
+def run(epoch: int, device: str, batch_size: int, path: str):
     if not Path(path).exists():
         print(f"Dataset not found in '{path}'")
         return
 
     trainer = Trainer(5)
     hyperparameter = Hyperparameter(
-        epoch=10,
+        epoch=epoch,
         learning_rate=0.001,
         batch_size_test=batch_size * 2,
         batch_size_train=batch_size,
@@ -23,12 +23,14 @@ def run(device: str, batch_size: int, path: str):
 
 if __name__ == "__main__":
     parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--epoch", default=50, type=int)
     parser.add_argument("-m", "--mode", default="cpu", type=str)
     parser.add_argument("-bs", "--batchsize", default=1, type=int)
     parser.add_argument("-p", "--path", required=True, type=str)
 
     parsed_data = parser.parse_args()
     run(
+        epoch=parsed_data.epoch,
         device=parsed_data.mode,
         batch_size=parsed_data.batchsize,
         path=parsed_data.path,

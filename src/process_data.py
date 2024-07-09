06776190e7dc8d8604a31dd1faefc8e5624cfcf0
test <test@test.com>
2024-07-09 16:14:12 +0800

    Update process data

--- src/process_data.py
+++ src/process_data.py
@@ -70,25 +70,24 @@ class CardiacDataProcessor:
 
     @staticmethod
     def generate_mask(rle_lung_left, rle_lung_right, rle_heart, height, width):
-        output_image = torch.zeros([height, width, 3], dtype=torch.uint8)
         mask_lung_left = CardiacDataProcessor.rle_to_mask(
             rle_lung_left, height=height, width=width
         )
         mask_lung_right = CardiacDataProcessor.rle_to_mask(
             rle_lung_right, height=height, width=width
         )
-        mask_heart = CardiacDataProcessor.rle_to_mask(
-            rle_heart, height=height, width=width
+        mask_heart = CardiacDataProcessor.rle_to_mask(rle_heart, height=height, width=width)
+        mask_lung = torch.clamp(
+            (mask_lung_left + mask_lung_right),
+            min=0,
+            max=255,
         )
-        output_image[((mask_lung_left + mask_lung_right) == 255)] = torch.tensor(
-            [128, 0, 0],
-            dtype=torch.uint8,
+        mask_background = torch.clamp(
+            (mask_heart + mask_lung),
+            min=0,
+            max=255,
         )
-        output_image[mask_heart == 255] = torch.tensor(
-            [128, 64, 128],
-            dtype=torch.uint8,
-        )
-        return output_image.permute([2, 0, 1])
+        return torch.stack([mask_background, mask_lung, mask_lung])
 
     @staticmethod
     def _process(job: JobData):
@@ -151,151 +150,151 @@ class CardiacDataProcessor:
                 pass
 
 
-class UavidDatasetProcessor:
-    def __init__(
-        self,
-        path,
-        output_directory="data/processed_dataset",
-        is_train=True,
-    ):
-        directory = Path(path)
-        self.output_directory = Path(output_directory)
-        self.is_train = is_train
+# class UavidDatasetProcessor:
+#     def __init__(
+#         self,
+#         path,
+#         output_directory="data/processed_dataset",
+#         is_train=True,
+#     ):
+#         directory = Path(path)
+#         self.output_directory = Path(output_directory)
+#         self.is_train = is_train
 
-        if not self.output_directory.exists():
-            self.output_directory.mkdir()
+#         if not self.output_directory.exists():
+#             self.output_directory.mkdir()
 
-        if self.is_train:
-            self.images = directory.glob("uavid_train/**/Images/*.png")
-            self.labels = directory.glob("uavid_train/**/Labels/*.png")
-        else:
-            self.images = directory.glob("uavid_val/**/Images/*.png")
-            self.labels = directory.glob("uavid_val/**/Labels/*.png")
+#         if self.is_train:
+#             self.images = directory.glob("uavid_train/**/Images/*.png")
+#             self.labels = directory.glob("uavid_train/**/Labels/*.png")
+#         else:
+#             self.images = directory.glob("uavid_val/**/Images/*.png")
+#             self.labels = directory.glob("uavid_val/**/Labels/*.png")
 
-        # if len(self.images) is not len(self.labels):
-        #     print("Number of images & label are not the same.")
-        #     return
+#         # if len(self.images) is not len(self.labels):
+#         #     print("Number of images & label are not the same.")
+#         #     return
 
-    @staticmethod
-    def decode_image(image_path):
-        return read_image(image_path)
+#     @staticmethod
+#     def decode_image(image_path):
+#         return read_image(image_path)
 
-    @staticmethod
-    def encode_image(image):
-        return encode_png(image)
+#     @staticmethod
+#     def encode_image(image):
+#         return encode_png(image)
 
-    @staticmethod
-    def resize_image(image):
-        return resize(
-            image,
-            size=[2160, 3840],
-            interpolation=InterpolationMode.BICUBIC,
-            antialias=True,
-        )
+#     @staticmethod
+#     def resize_image(image):
+#         return resize(
+#             image,
+#             size=[2160, 3840],
+#             interpolation=InterpolationMode.BICUBIC,
+#             antialias=True,
+#         )
 
-    @staticmethod
-    def crop_256(image, label):
-        img_array = []
-        label_array = []
+#     @staticmethod
+#     def crop_256(image, label):
+#         img_array = []
+#         label_array = []
 
-        blocks = [
-            (0, 1024, 0, 2048),
-            (0, 1024, 896, 2944),
-            (0, 1024, 1792, 3840),
-            (568, 1592, 0, 2048),
-            (568, 1592, 896, 2944),
-            (568, 1592, 1792, 3840),
-            (1136, 2160, 0, 2048),
-            (1136, 2160, 896, 2944),
-            (1136, 2160, 1792, 3840),
-        ]
-        for y_min, _, x_min, _ in blocks:
-            for index in range(16):
-                y, x = index // 8, index % 8
-                block_y_min = y_min + (y * 256)
-                block_y_max = y_min + (y + 1) * 256
-                block_x_min = x_min + x * 256
-                block_x_max = x_min + (x + 1) * 256
-                img_array.append(
-                    image[::, block_y_min:block_y_max, block_x_min:block_x_max]
-                )
-                label_array.append(
-                    label[::, block_y_min:block_y_max, block_x_min:block_x_max]
-                )
+#         blocks = [
+#             (0, 1024, 0, 2048),
+#             (0, 1024, 896, 2944),
+#             (0, 1024, 1792, 3840),
+#             (568, 1592, 0, 2048),
+#             (568, 1592, 896, 2944),
+#             (568, 1592, 1792, 3840),
+#             (1136, 2160, 0, 2048),
+#             (1136, 2160, 896, 2944),
+#             (1136, 2160, 1792, 3840),
+#         ]
+#         for y_min, _, x_min, _ in blocks:
+#             for index in range(16):
+#                 y, x = index // 8, index % 8
+#                 block_y_min = y_min + (y * 256)
+#                 block_y_max = y_min + (y + 1) * 256
+#                 block_x_min = x_min + x * 256
+#                 block_x_max = x_min + (x + 1) * 256
+#                 img_array.append(
+#                     image[::, block_y_min:block_y_max, block_x_min:block_x_max]
+#                 )
+#                 label_array.append(
+#                     label[::, block_y_min:block_y_max, block_x_min:block_x_max]
+#                 )
 
-        return img_array, label_array
+#         return img_array, label_array
 
-    @staticmethod
-    def generate_new_name(root, path, number):
-        folder_name = str(root).split("/")[-3]
-        index = path.name.replace(r".png", "")
-        number_string = str(number)
-        return (
-            f"{folder_name}_{index}_0{str(number_string)}.png"
-            if len(number_string) == 1
-            else f"{folder_name}_{index}_{str(number_string)}.png"
-        )
+#     @staticmethod
+#     def generate_new_name(root, path, number):
+#         folder_name = str(root).split("/")[-3]
+#         index = path.name.replace(r".png", "")
+#         number_string = str(number)
+#         return (
+#             f"{folder_name}_{index}_0{str(number_string)}.png"
+#             if len(number_string) == 1
+#             else f"{folder_name}_{index}_{str(number_string)}.png"
+#         )
 
-    @staticmethod
-    def _process(job: JobData):
-        image_path, label_path, output_directory = (
-            job.image_path,
-            job.label,
-            job.output_directory,
-        )
+#     @staticmethod
+#     def _process(job: JobData):
+#         image_path, label_path, output_directory = (
+#             job.image_path,
+#             job.label,
+#             job.output_directory,
+#         )
 
-        image = UavidDatasetProcessor.decode_image(str(image_path))
-        image = UavidDatasetProcessor.resize_image(image)
-        label = UavidDatasetProcessor.decode_image(str(label_path))
-        label = UavidDatasetProcessor.resize_image(label)
+#         image = UavidDatasetProcessor.decode_image(str(image_path))
+#         image = UavidDatasetProcessor.resize_image(image)
+#         label = UavidDatasetProcessor.decode_image(str(label_path))
+#         label = UavidDatasetProcessor.resize_image(label)
 
-        img_array, label_array = UavidDatasetProcessor.crop_256(
-            image=image, label=label
-        )
-        for index in range(len(img_array)):
-            new_image_path = output_directory.joinpath("image").joinpath(
-                UavidDatasetProcessor.generate_new_name(
-                    str(image_path.absolute()), image_path, number=index
-                )
-            )
-            new_label_path = output_directory.joinpath("label").joinpath(
-                UavidDatasetProcessor.generate_new_name(
-                    str(image_path.absolute()), image_path, number=index
-                )
-            )
-            jpeg_image = UavidDatasetProcessor.encode_image(img_array[index])
-            jpeg_label = UavidDatasetProcessor.encode_image(label_array[index])
+#         img_array, label_array = UavidDatasetProcessor.crop_256(
+#             image=image, label=label
+#         )
+#         for index in range(len(img_array)):
+#             new_image_path = output_directory.joinpath("image").joinpath(
+#                 UavidDatasetProcessor.generate_new_name(
+#                     str(image_path.absolute()), image_path, number=index
+#                 )
+#             )
+#             new_label_path = output_directory.joinpath("label").joinpath(
+#                 UavidDatasetProcessor.generate_new_name(
+#                     str(image_path.absolute()), image_path, number=index
+#                 )
+#             )
+#             jpeg_image = UavidDatasetProcessor.encode_image(img_array[index])
+#             jpeg_label = UavidDatasetProcessor.encode_image(label_array[index])
 
-            new_image_path.write_bytes(jpeg_image.numpy())
-            new_label_path.write_bytes(jpeg_label.numpy())
+#             new_image_path.write_bytes(jpeg_image.numpy())
+#             new_label_path.write_bytes(jpeg_label.numpy())
 
-    def process(self):
-        output_image_path = (
-            self.output_directory.joinpath("train")
-            if self.is_train
-            else self.output_directory.joinpath("test")
-        )
-        if not output_image_path.exists():
-            output_image_path.mkdir()
+#     def process(self):
+#         output_image_path = (
+#             self.output_directory.joinpath("train")
+#             if self.is_train
+#             else self.output_directory.joinpath("test")
+#         )
+#         if not output_image_path.exists():
+#             output_image_path.mkdir()
 
-        new_image_path = output_image_path.joinpath("image")
-        new_label_path = output_image_path.joinpath("label")
-        if not new_image_path.exists():
-            new_image_path.mkdir()
-        if not new_label_path.exists():
-            new_label_path.mkdir()
+#         new_image_path = output_image_path.joinpath("image")
+#         new_label_path = output_image_path.joinpath("label")
+#         if not new_image_path.exists():
+#             new_image_path.mkdir()
+#         if not new_label_path.exists():
+#             new_label_path.mkdir()
 
-        images = [x for x in self.images]
-        labels = [x for x in self.labels]
-        jobs_data = [
-            JobData(image, label, output_image_path)
-            for image, label in zip(images, labels)
-        ]
+#         images = [x for x in self.images]
+#         labels = [x for x in self.labels]
+#         jobs_data = [
+#             JobData(image, label, output_image_path)
+#             for image, label in zip(images, labels)
+#         ]
 
-        total_len = len(jobs_data)
-        with ProcessPoolExecutor() as executor:
-            for index in tqdm(executor.map(self._process, jobs_data)):
-                pass
+#         total_len = len(jobs_data)
+#         with ProcessPoolExecutor() as executor:
+#             for index in tqdm(executor.map(self._process, jobs_data)):
+#                 pass
 
 
 def process_images(path: str):

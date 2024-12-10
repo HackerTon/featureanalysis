e2f544e79ed8f994faf548ec0c403bc8186674b8
test <test@test.com>
2024-12-10 19:27:53 +0800

    Cache he_dataset

--- src/dataloader/dataset/he_dataset.py
+++ src/dataloader/dataset/he_dataset.py
@@ -1,51 +1,99 @@
 from pathlib import Path
 
 import torch
+from itertools import chain
 from torch.utils.data import Dataset
-from torchvision.io.image import ImageReadMode, read_image
-from torchvision.transforms.functional import resize
+from torchvision.io.image import ImageReadMode, read_image, write_png
+from torchvision.transforms.functional import resize, InterpolationMode
 
 
 class HeDataset(Dataset):
     def __init__(self, directory_path: str):
         self.directory = Path(directory_path)
+        self.cache_directory = Path("data/he_dataset")
         self.labels = []
         self.images = []
 
-        self.images = [str(x) for x in self.directory.glob("imagesTr/*.png")]
-        self.images.extend([str(x) for x in self.directory.glob("imagesTs/*.png")])
-        for image_path in self.images:
-            label_path = "_".join(image_path.split("_")[:-1]) + ".png"
+        self._initialize_dataset()
+
+    def _initialize_dataset(self):
+        train_images = list(self.directory.glob("imagesTr/*.png"))
+        test_images = list(self.directory.glob("imagesTs/*.png"))
+        total_num_samples = 4 * 4 * (len(train_images) + len(test_images))
+
+        # Check the number of images in cache
+        # If complete, return
+        if self.cache_directory.exists():
+            num_images = len(list(self.cache_directory.glob("*image.png")))
+            num_labels = len(list(self.cache_directory.glob("*label.png")))
+
+            if num_images == num_labels == total_num_samples:
+                self.dataset_length = total_num_samples
+                return
+
+        self.cache_directory.mkdir(exist_ok=True)
+        self._cache_dataset(chain(train_images, test_images))
+        self.dataset_length = total_num_samples
+
+    def _cache_dataset(self, images):
+        count = 0
+        for image_path in images:
+            label_path = "_".join(str(image_path).split("_")[:-1]) + ".png"
             label_path = label_path.replace("imagesTr", "labelsTr")
             label_path = label_path.replace("imagesTs", "labelsTs")
-            self.labels.append(label_path)
+
+            image = read_image(str(image_path), ImageReadMode.RGB)
+            label = read_image(label_path, ImageReadMode.GRAY)
+
+            resized_image = resize(image, [1024, 1024], InterpolationMode.NEAREST)
+            resized_label = resize(label, [1024, 1024], InterpolationMode.NEAREST)
+
+            for i in range(1, 5):
+                for j in range(1, 5):
+                    y_slice = slice((i - 1) * 256, i * 256)
+                    x_slice = slice((j - 1) * 256, j * 256)
+
+                    block_image = resized_image[..., y_slice, x_slice]
+                    block_label = resized_label[..., y_slice, x_slice]
+
+                    write_png(
+                        block_image,
+                        str(self.cache_directory.joinpath(f"{count}_image.png")),
+                    )
+                    write_png(
+                        block_label,
+                        str(self.cache_directory.joinpath(f"{count}_label.png")),
+                    )
+                    count += 1
 
     def __len__(self):
-        return len(self.images)
+        return self.dataset_length
 
     def get_mask(self, label):
-        return torch.concatenate(
-            [
-                (label == 0),
-                (label == 1),
-                (label == 2),
-            ]
-        ) * 255
+        return (
+            torch.concatenate(
+                [
+                    (label == 0),
+                    (label == 1),
+                    (label == 2),
+                ]
+            )
+            * 255
+        )
 
     def __getitem__(self, index):
-        image = read_image(self.images[index], ImageReadMode.RGB)
-        label = read_image(self.labels[index], ImageReadMode.GRAY)
-        label = self.get_mask(label)
-
-        resized_image = resize(image, [1080, 1920]).to(torch.uint8)
-        resized_label = resize(label, [1080, 1920]).to(torch.uint8)
-        return resized_image, resized_label
+        image_path = str(self.cache_directory.joinpath(f"{index}_image.png"))
+        label_path = str(self.cache_directory.joinpath(f"{index}_label.png"))
+        image = read_image(image_path, ImageReadMode.RGB)
+        label = read_image(label_path, ImageReadMode.GRAY)
+        label = self.get_mask(label).to(torch.uint8)
+        return image, label
 
 
 if __name__ == "__main__":
     from torchvision.io import write_jpeg
 
-    dataset = HeDataset("/Users/babi/Documents/dataset/Dataset110_Tiger")
+    dataset = HeDataset("/mnt/storage/Dataset110_Tiger")
     i = 0
     for image, mask in dataset:
         write_jpeg(image, "image.jpg")
